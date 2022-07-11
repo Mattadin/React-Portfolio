43d42ebddd0a667a37d8bbc6eac937ce9059b1f4
Mattadin <m4tt4d1n@gmail.com>
2022-07-11 16:24:15 +1000

    Header components added

--- -p/src/components/header/Header.jsx
+++ -p/src/components/header/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import './header.css';
+import CTA from './CTA';
+import PROFILE from '../../assets/Mattadin.jpg';
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello, I am</h5>
+        <h1>Matthew Fernandez</h1>
+        <h5 className="text-light">Fullstack Web Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="profile">
+          <img src={PROFILE} alt="Matthew" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
