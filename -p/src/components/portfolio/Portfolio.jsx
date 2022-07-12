320e0029c20e3fdfb61bffb2e745cf516d263d40
Mattadin <m4tt4d1n@gmail.com>
2022-07-12 17:33:58 +1000

    portfolio component added

--- -p/src/components/portfolio/Portfolio.jsx
+++ -p/src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,79 @@
 import React from 'react';
 import './portfolio.css';
+import IMG1 from '../../assets/portfolio-image-1.png';
+import IMG2 from '../../assets/portfolio-image-2.png';
+import IMG3 from '../../assets/portfolio-image-3.png';
+import IMG4 from '../../assets/portfolio-image-4.png';
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Insert title here',
+    github: 'https://github.com',
+    demo: 'https://github.com',
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Insert title here',
+    github: 'https://github.com',
+    demo: 'https://github.com',
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Insert title here',
+    github: 'https://github.com',
+    demo: 'https://github.com',
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Insert title here',
+    github: 'https://github.com',
+    demo: 'https://github.com',
+  },
+];
 
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2> Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a
+                  href={github}
+                  rel="noreferrer"
+                  className="btn"
+                  target="_blank"
+                >
+                  Github
+                </a>
+                <a
+                  href={demo}
+                  rel="noreferrer"
+                  className="btn btn-primary"
+                  target="_blank"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
